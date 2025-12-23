98bccf0b8f90c2cd308ee8602fab72878092c4ed
Arthur Miguel <110751433+Arthurmiguel577@users.noreply.github.com>
2025-12-22 22:38:23 -0300

    feat: add gallery pages and youtube video support, remove large video files

---
+++ js/db.js
@@ -0,0 +1,35 @@
+const mediaDB = {
+    photos: [
+        // Destaques (Root)
+        { src: "../imgs/emilly-bolo.png", category: "destaque" },
+        { src: "../imgs/emilly-danca-pose.png", category: "destaque" },
+        { src: "../imgs/emilly-danca-primas.png", category: "destaque" },
+        { src: "../imgs/emilly-decoracao-2.png", category: "destaque" },
+        { src: "../imgs/emilly-decoracao-bolo.png", category: "destaque" },
+        { src: "../imgs/emilly-decoracao-mesa.png", category: "destaque" },
+        { src: "../imgs/emilly-entrada-magico.png", category: "destaque" },
+        { src: "../imgs/emilly-magico.png", category: "destaque" },
+        { src: "../imgs/emilly-oracao.png", category: "destaque" },
+        { src: "../imgs/emilly-parabens.png", category: "destaque" },
+        { src: "../imgs/emilly-robo-dancante-1.png", category: "destaque" },
+        { src: "../imgs/emilly-robo-dancante-2.png", category: "destaque" },
+        { src: "../imgs/emilly-robo-dancante-3.png", category: "destaque" },
+        { src: "../imgs/emilly-robo-dancante-4.png", category: "destaque" },
+        { src: "../imgs/emilly-robo-dancante.png", category: "destaque" },
+        { src: "../imgs/emilly-robo-festa.jpg", category: "destaque" },
+        { src: "../imgs/emilly-valsa-1.png", category: "destaque" },
+        { src: "../imgs/emilly-valsa-2.png", category: "destaque" },
+        { src: "../imgs/emilly-valsa-3.png", category: "destaque" },
+        { src: "../imgs/emilly-valsa-4.png", category: "destaque" },
+        { src: "../imgs/emilly-valsa-5.png", category: "destaque" },
+        // Extras
+        { src: "../imgs/extras/IMG_20251220_223402.jpg", category: "extra" },
+        { src: "../imgs/extras/IMG_20251220_223654.jpg", category: "extra" },
+        { src: "../imgs/extras/IMG_20251220_223656.jpg", category: "extra" },
+        { src: "../imgs/extras/IMG_20251220_223657.jpg", category: "extra" }
+    ],
+    videos: [
+        // Vídeos removidos temporariamente para otimização de deploy.
+        // Adicione links do YouTube/Vimeo aqui futuramente.
+    ]
+};
